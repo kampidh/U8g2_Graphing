b400926870e081c17d5fb9daab71bf2b1d388670
kampidh <qampidh@hotmail.com>
2024-05-18 18:07:39 +0700

    Formatting: tidying up whitespace

--- src/U8g2Graphing.h
+++ src/U8g2Graphing.h
@@ -1,9 +1,8 @@
 /*
-	U8g2Graphing - a simple graphing function for U8g2 library
-	Kampidh 2020
-	
-	Licensed under MIT license
-	
+    U8g2Graphing - a simple graphing function for U8g2 library
+    Kampidh 2020
+
+    Licensed under MIT license
 */
 
 #ifndef U8G2GRAPH_H
@@ -14,7 +13,7 @@
 
 class U8g2Graphing
 {
-  public:
+public:
     U8g2Graphing(U8G2 *u8g);
     ~U8g2Graphing();
     void begin(uint16_t fromx, uint16_t fromy, uint16_t tox, uint16_t toy);
@@ -28,16 +27,17 @@ class U8g2Graphing
     void pointerSetI(bool pointer, uint16_t pointndx = 0);
     void rangeSet(bool setrange, float vmin = 0, float vmax = 0);
     void inputValue(float var);
-	void inputValue(int var);
-	void clearData();
+    void inputValue(int var);
+    void clearData();
     uint16_t getDataLen();
     float getMin();
     float getMax();
-	float getDataMin();
+    float getDataMin();
     float getDataMax();
     void displayGraph();
-  private:
-	void inValue(float var);
+
+private:
+    void inValue(float var);
     void inValue(int var);
     float fmap(float x, float in_min, float in_max, float out_min, float out_max);
     uint16_t **graph;
